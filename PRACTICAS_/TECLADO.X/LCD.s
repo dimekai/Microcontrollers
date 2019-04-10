597a64e071e3da6f99514a8d5d9b21d12a14f7cf
JesusDiaz08 <jesus_kdm@hotmail.com>
2019-04-09 20:39:31 -0500

    Practica TIME : Teclado con Notas Musicales :musical_keyboard:

---
+++ PRACTICAS_/TECLADO.X/LCD.s
@@ -0,0 +1,339 @@
+    ; |------------ CABECERA ------------|
+    .include "p30F4013.inc" ; Aqui estan todos los registros del micro.
+        
+    ; |------------ FUNCIONES LCD ------------|
+    ; Estas funciones son de los diagramas que definimos
+    .GLOBAL _comandoLCD	    
+    .GLOBAL _datoLCD
+    .GLOBAL _busyFlagLCD
+    .GLOBAL _iniLCD8bits
+    .GLOBAL _imprimeLCD
+    
+    ; |------------ EQUIVALENCIAS ------------|
+    ; Este es el equivalente a definicion de macros (#define) en C
+    .EQU    RS_LCD,	RF2 ; RS
+    .EQU    RW_LCD,	RF3 ; RW
+    .EQU    ENABLE_LCD, RD2 ; ENABLE
+    .EQU    BF_LCD,	RB7 ; BF: BUSY_FLAG
+    
+    ; |------------ FUNCION DE INICIALIZACIÓN ------------|
+    .GLOBAL _iniInterrupciones	; Esta en C
+    .GLOBAL _T1Interrupt	; ISR_T1
+    
+    ; |=============== NOTAS MUSICALES ===============|
+    .GLOBAL _NOTA_DO	    ; preescalar = 64
+    .GLOBAL _NOTA_RE	    ; preescalar = 64
+    .GLOBAL _NOTA_MI	    ; preescalar = 256
+    .GLOBAL _NOTA_FA	    ; preescalar = 1
+    .GLOBAL _NOTA_SOL	    ; preescalar = 1
+    .GLOBAL _NOTA_LA	    ; preescalar = 8
+    .GLOBAL _NOTA_SI	    ; preescalar = 1
+    
+    .GLOBAL _MENSAJE_DO
+    .GLOBAL _MENSAJE_RE
+    .GLOBAL _MENSAJE_MI
+    .GLOBAL _MENSAJE_FA
+    .GLOBAL _MENSAJE_SOL
+    .GLOBAL _MENSAJE_LA
+    .GLOBAL _MENSAJE_SI
+    
+; |------------------ FUNCION COMANDO_LCD ------------------|
+_comandoLCD:
+    CLR	    TRISD
+    NOP
+    
+    BCLR    PORTF, #RS_LCD	; RS = 0
+    NOP
+    BCLR    PORTF, #RW_LCD	; RW = 0
+    NOP
+    BSET    PORTD, #ENABLE_LCD	; ENABLE = 1
+    NOP
+    
+    MOV.B   WREG,   PORTB	; PORTB = W0
+    NOP
+
+    BCLR    PORTD, #ENABLE_LCD	; ENABLE = 0
+    NOP
+  
+    RETURN
+	    
+; |------------------- FUNCION DATO LCD -------------------|    
+_datoLCD:
+    CLR	    TRISF
+    CLR	    TRISD
+    
+    BSET    PORTF, #RS_LCD	;   RS = 1
+    NOP
+    BCLR    PORTF, #RW_LCD	;   RW = 0
+    NOP
+    BSET    PORTD, #ENABLE_LCD	;   ENABLE = 1
+    NOP
+    
+    MOV.B   WREG,   PORTB	;   PORTB = W0
+    NOP
+
+    BCLR    PORTD, #ENABLE_LCD	;   ENABLE = 0
+    NOP
+  
+    RETURN    
+
+; |------------------- FUNCION BUSY_FLAG -------------------|   
+_busyFlagLCD:
+    PUSH    W0
+    CLR	    TRISF
+    CLR	    TRISD
+    
+    BCLR    PORTF,  #RS_LCD	;   RS = 0
+    NOP
+
+    SETM.B  TRISB		;   Prendemos la parte baja - TRISB OR 0X00FF
+    NOP
+
+    BSET    PORTF,  #RW_LCD	;   RW = 1
+    NOP
+
+    BSET    PORTD,  #ENABLE_LCD	;   ENABLE = 1
+    NOP
+
+PROCESO:
+    BTSC	PORTB,	#BF_LCD	;   VERIFICA SI BF = 0, SI NO, SE EJECUTA EL GOTO
+    GOTO	PROCESO
+    
+    BCLR	PORTD,	#ENABLE_LCD ;	ENABLE = 0
+    NOP
+    BCLR	PORTF, #RW_LCD	    ;   RW = 0
+    NOP
+    
+    SETM	TRISB		    ; TRISB = 0xFFFF
+    NOP
+    CLR.B	TRISB		    ; TRISB = 0xFF00 ; APAGA LA PARTE BAJA
+    NOP
+    
+    POP	    W0
+    RETURN;
+    
+; |------------------- FUNCION INICIALIZAR LCD DE 8 BITS -------------------|
+; | ---- INICIALIZACION ---|
+; | D7 | D6 | D5 | D4 | D3 |  D2 |  D1 |  D0 |     COMANDO    | CODIGO |
+; | 0  | 0  | 1  | 1  | X  |  X  |  X  |  X  |   FUNCION SET  |  0X30  |    ; RETARDO 01
+; | 0  | 0  | 1  | 1  | X  |  X  |  X  |  X  |   FUNCION SET  |  0X30  |    ; RETARDO 02
+; | 0  | 0  | 1  | 1  | X  |  X  |  X  |  X  |   FUNCION SET  |  0X30  |    ; RETARDO 03
+; | ---- CONFIGURACION ----|
+; | 0  | 0  | 1  | 1  | N=1| F=0 |  X  |  X  |   FUNCION SET  |  0X38  |
+; | 0  | 0  | 0  | 0  | 1  | D=0 | C=0 | B=0 | DISPLAY ON/OFF |  0X08  |
+; | 0  | 0  | 0  | 0  | 0  |  0  |  0  |  1  |  CLEAR DISPLAY |  0X01  |
+; | 0  | 0  | 0  | 0  | 0  |  1  |I/D=1| S=0 | ENTRY MODE SET |  0X06  |
+; | 0  | 0  | 0  | 0  | 1  | D=1 | C=1 | B=1 | DISPLAY ON/OFF |  0X0F  |
+    
+_iniLCD8bits:
+    CLR	    W0
+    ; ------- TABLA DE INICIALIZACION -------------
+    CALL    RETARDO_15ms	; -- RETARDO 01
+    MOV	    #0X30,  W0
+    CALL    _comandoLCD
+
+    CALL    RETARDO_15ms	; -- RETARDO 02
+    MOV	    #0X30,  W0
+    CALL    _comandoLCD
+    
+    CALL    RETARDO_15ms	; -- RETARDO 03
+    MOV	    #0X30,  W0
+    CALL    _comandoLCD
+    
+    ; ------- TABLA DE CONFIGURACION ---------------
+    CALL    _busyFlagLCD
+    MOV	    #0X38,  W0	    ;	CODIGO: 0X38 - FUNCTION SET
+    CALL    _comandoLCD
+    
+    CALL    _busyFlagLCD
+    MOV	    #0X08,  W0	    ;	CODIGO: 0X08 - DISPLAY ON/OFF
+    CALL    _comandoLCD
+    
+    CALL    _busyFlagLCD
+    MOV	    #0X01,  W0	    ;	CODIGO: 0X01 - CLEAR DISPLAY
+    CALL    _comandoLCD
+    
+    CALL    _busyFlagLCD
+    MOV	    #0X06,  W0	    ;	CODIGO: 0X06 - ENTRY MODE SET
+    CALL    _comandoLCD
+    
+    CALL    _busyFlagLCD
+    MOV	    #0X0F,  W0	    ;	CODIGO: 0X0F - DISPLAY ON/OFF
+    CALL    _comandoLCD
+    
+    RETURN
+
+; |=============== RETARDO ============|
+; @brief: Genera un retardo de 15ms
+RETARDO_15ms:
+    PUSH    W0
+    PUSH    W1  
+    CLR     W0
+CICLO1_1S:
+    DEC     W0,     W0
+    BRA     NZ,     CICLO1_1S
+
+    POP     W1
+    POP     W0
+    return
+
+; |================ ISR_T1 ================|
+; @brief: Genera el reloj por software.
+_T1Interrupt:
+    BTG	    LATD,   #LATD8
+    NOP
+    BCLR    IFS0,   #T1IF
+    RETFIE    
+    
+; |================== IMPRIMI EN LCD ==================|
+; |@brief: Imprime en el LCD una cadena de caracteres
+; |@param: Cadena de caracteres
+_imprimeLCD:
+    ; Se manda la direccion asociada a la variable
+    ; A partir de esto, utilizamos [] para tomar la direccion que se manda
+    PUSH    W1 
+    MOV	    W0,	    W1
+CICLO:
+    MOV.B   [W1++], W0	    ; Se utiliza .b ya que cada elemento, cada letra, es un byte
+    CP0.B   W0		    ; Compara el primer byte de W0 con 0
+    BRA	    Z, FIN	    ; Si ya llego al final, si es nulo, terminamos el programa
+    CALL    _busyFlagLCD
+    CALL    _datoLCD
+    GOTO    CICLO
+FIN:
+    POP	    W1
+    RETURN
+    
+    
+; |=============== NOTAS MUSICALES ===============|
+_NOTA_DO:
+    
+    CLR	    TMR1		; TMR1 = 0
+    MOV	    #55,	W0	; 
+    MOV	    W0,		PR1	; PR1 = 55
+    MOV	    #0x8020,	W0	
+    MOV	    W0,		T1CON	; T1CON = 0x8020
+    
+    CALL    _busyFlagLCD
+    
+    CALL    _comandoLCD
+    CALL    _busyFlagLCD
+    MOV	    #_MENSAJE_DO, W0
+    CALL    _imprimeLCD
+    
+    
+    RETURN
+
+_NOTA_RE:
+    
+    CLR	    TMR1		; TMR1 = 0
+    MOV	    #49,	W0	; 
+    MOV	    W0,		PR1	; PR1 = 49
+    MOV	    #0x8020,	W0	
+    MOV	    W0,		T1CON	; T1CON = 0x8020
+    
+    CALL    _busyFlagLCD
+    
+    CALL    _comandoLCD
+    CALL    _busyFlagLCD
+    MOV	    #_MENSAJE_RE, W0
+    CALL    _imprimeLCD
+    
+    
+    RETURN
+    
+_NOTA_MI:
+    
+    CLR	    TMR1		; TMR1 = 0
+    MOV	    #11,	W0	; 
+    MOV	    W0,		PR1	; PR1 = 11
+    MOV	    #0x8030,	W0	
+    MOV	    W0,		T1CON	; T1CON = 0x8030
+    
+    CALL    _busyFlagLCD
+    
+    CALL    _comandoLCD
+    CALL    _busyFlagLCD
+    MOV	    #_MENSAJE_MI, W0
+    CALL    _imprimeLCD
+    
+    
+    RETURN    
+
+_NOTA_FA:
+    
+    CLR	    TMR1		; TMR1 = 0
+    MOV	    #2639,	W0	; 
+    MOV	    W0,		PR1	; PR1 = 2639
+    MOV	    #0x8000,	W0	
+    MOV	    W0,		T1CON	; T1CON = 0x8000
+    
+    CALL    _busyFlagLCD
+    
+    CALL    _comandoLCD
+    CALL    _busyFlagLCD
+    MOV	    #_MENSAJE_FA, W0
+    CALL    _imprimeLCD
+    
+    
+    RETURN
+
+_NOTA_SOL:
+    
+    CLR	    TMR1		; TMR1 = 0
+    MOV	    #2351,	W0	; 
+    MOV	    W0,		PR1	; PR1 = 2351
+    MOV	    #0x8000,	W0	
+    MOV	    W0,		T1CON	; T1CON = 0x8000
+    
+    CALL    _busyFlagLCD
+    
+    CALL    _comandoLCD
+    CALL    _busyFlagLCD
+    MOV	    #_MENSAJE_SOL, W0
+    CALL    _imprimeLCD
+    
+    
+    RETURN
+    
+_NOTA_LA:
+    
+    CLR	    TMR1		; TMR1 = 0
+    MOV	    #262,	W0	; 
+    MOV	    W0,		PR1	; PR1 = 262
+    MOV	    #0x8010,	W0	
+    MOV	    W0,		T1CON	; T1CON = 0x8010
+    
+    CALL    _busyFlagLCD
+    
+    CALL    _comandoLCD
+    CALL    _busyFlagLCD
+    MOV	    #_MENSAJE_LA, W0
+    CALL    _imprimeLCD
+    
+    
+    RETURN
+
+_NOTA_SI:
+    
+    CLR	    TMR1		; TMR1 = 0
+    MOV	    #1866,	W0	; 
+    MOV	    W0,		PR1	; PR1 = 1866
+    MOV	    #0x8000,	W0	
+    MOV	    W0,		T1CON	; T1CON = 0x8000
+    
+    CALL    _busyFlagLCD
+    
+    CALL    _comandoLCD
+    CALL    _busyFlagLCD
+    MOV	    #_MENSAJE_SI, W0
+    CALL    _imprimeLCD
+    
+    
+    RETURN    
+    
+_iniInterrupciones:
+    BCLR IFS0,	    #INT1IF
+    BSET IEC0,	    #INT1IE
+    return
+    
---
+++ PRACTICAS_/TECLADO.X/principal.c
@@ -0,0 +1,169 @@
+/**@brief: Este programa muestra los bloques de un 
+ * programa en C embebido para el DSPIC, los bloques son:
+ * BLOQUE 1. OPCIONES DE CONFIGURACION DEL DSC: OSCILADOR, WATCHDOG,
+ * BROWN OUT RESET, POWER ON RESET Y CODIGO DE PROTECCION
+ * BLOQUE 2. EQUIVALENCIAS Y DECLARACIONES GLOBALES
+ * BLOQUE 3. ESPACIOS DE MEMORIA: PROGRAMA, DATOS X, DATOS Y, DATOS NEAR
+ * BLOQUE 4. CÓDIGO DE APLICACIÓN
+ * @device: DSPIC30F4013
+ * @oscillator: FRC, 7.3728MHz
+ */
+#include "p30F4013.h"
+/********************************************************************************/
+/* 						BITS DE CONFIGURACIÓN									*/	
+/********************************************************************************/
+/* SE DESACTIVA EL CLOCK SWITCHING Y EL FAIL-SAFE CLOCK MONITOR (FSCM) Y SE 	*/
+/* ACTIVA EL OSCILADOR INTERNO (FAST RC) PARA TRABAJAR							*/
+/* FSCM: PERMITE AL DISPOSITIVO CONTINUAR OPERANDO AUN CUANDO OCURRA UNA FALLA 	*/
+/* EN EL OSCILADOR. CUANDO OCURRE UNA FALLA EN EL OSCILADOR SE GENERA UNA 		*/
+/* TRAMPA Y SE CAMBIA EL RELOJ AL OSCILADOR FRC  								*/
+/********************************************************************************/
+//_FOSC(CSW_FSCM_OFF & FRC); 
+//SON DIRECTIVAS DE CONFIGURACIÓN.
+#pragma config FOSFPR = FRC             // Oscillator (Internal Fast RC (No change to Primary Osc Mode bits))
+#pragma config FCKSMEN = CSW_FSCM_OFF   // Clock Switching and Monitor (Sw Disabled, Mon Disabled)/********************************************************************************/
+/* SE DESACTIVA EL WATCHDOG														*/
+/********************************************************************************/
+//_FWDT(WDT_OFF); 
+#pragma config WDT = WDT_OFF            // Watchdog Timer (Disabled)
+/********************************************************************************/
+/* SE ACTIVA EL POWER ON RESET (POR), BROWN OUT RESET (BOR), 					*/	
+/* POWER UP TIMER (PWRT) Y EL MASTER CLEAR (MCLR)								*/
+/* POR: AL MOMENTO DE ALIMENTAR EL DSPIC OCURRE UN RESET CUANDO EL VOLTAJE DE 	*/	
+/* ALIMENTACIÓN ALCANZA UN VOLTAJE DE UMBRAL (VPOR), EL CUAL ES 1.85V			*/
+/* BOR: ESTE MODULO GENERA UN RESET CUANDO EL VOLTAJE DE ALIMENTACIÓN DECAE		*/
+/* POR DEBAJO DE UN CIERTO UMBRAL ESTABLECIDO (2.7V) 							*/
+/* PWRT: MANTIENE AL DSPIC EN RESET POR UN CIERTO TIEMPO ESTABLECIDO, ESTO 		*/
+/* AYUDA A ASEGURAR QUE EL VOLTAJE DE ALIMENTACIÓN SE HA ESTABILIZADO (16ms) 	*/
+/********************************************************************************/
+//_FBORPOR( PBOR_ON & BORV27 & PWRT_16 & MCLR_EN ); 
+// FBORPOR
+#pragma config FPWRT  = PWRT_16          // POR Timer Value (16ms)
+#pragma config BODENV = BORV20           // Brown Out Voltage (2.7V)
+#pragma config BOREN  = PBOR_ON          // PBOR Enable (Enabled)
+#pragma config MCLRE  = MCLR_EN          // Master Clear Enable (Enabled)
+/********************************************************************************/
+/*SE DESACTIVA EL CÓDIGO DE PROTECCIÓN											*/
+/********************************************************************************/
+//_FGS(CODE_PROT_OFF);      
+// FGS
+#pragma config GWRP = GWRP_OFF          // General Code Segment Write Protect (Disabled)
+#pragma config GCP = CODE_PROT_OFF      // General Segment Code Protection (Disabled)
+
+/********************************************************************************/
+/* SECCIÓN DE DECLARACIÓN DE CONSTANTES CON DEFINE								*/
+/********************************************************************************/
+#define EVER 1
+#define MUESTRAS 64
+
+/********************************************************************************/
+/* DECLARACIONES GLOBALES														*/
+/********************************************************************************/
+/*DECLARACIÓN DE LA ISR DEL TIMER 1 USANDO __attribute__						*/
+/********************************************************************************/
+void __attribute__((__interrupt__)) _T1Interrupt( void );   // ESTO ES UNA INTERRUPCION
+
+/********************************************************************************/
+/* CONSTANTES ALMACENADAS EN EL ESPACIO DE LA MEMORIA DE PROGRAMA				*/
+/********************************************************************************/
+int ps_coeff __attribute__ ((aligned (2), space(prog)));    // DECLARAR UNA ARREGLO EN UNA ZONA ESPECIFICA.
+/********************************************************************************/
+/* VARIABLES NO INICIALIZADAS EN EL ESPACIO X DE LA MEMORIA DE DATOS			*/
+/********************************************************************************/
+int x_input[MUESTRAS] __attribute__ ((space(xmemory)));
+/********************************************************************************/
+/* VARIABLES NO INICIALIZADAS EN EL ESPACIO Y DE LA MEMORIA DE DATOS			*/
+/********************************************************************************/
+int y_input[MUESTRAS] __attribute__ ((space(ymemory)));
+/********************************************************************************/
+/* VARIABLES NO INICIALIZADAS LA MEMORIA DE DATOS CERCANA (NEAR), LOCALIZADA	*/
+/* EN LOS PRIMEROS 8KB DE RAM													*/
+/********************************************************************************/
+int var1 __attribute__ ((near));    // int var1;   es lo mismo
+
+
+void iniPerifericos( void );
+void iniInterrupciones( void );
+
+/* |----- FUNCIONES DECLARADAS EN ENSABLADOR ----| */
+void comandoLCD( char );
+void datoLCD( char );
+void busyFlagLCD( void );
+void iniLCD8bits( void );
+void imprimeLCD( char * );
+
+/* |===== FUNCIONES DE NOTAS MUSICALES =====|*/
+void NOTA_DO( void );
+void NOTA_RE( void );
+void NOTA_MI( void );
+void NOTA_FA( void );
+void NOTA_SOL( void );
+void NOTA_LA( void );
+void NOTA_SI( void );
+
+char * MENSAJE_DO = "NOTA DO";
+char * MENSAJE_DO = "NOTA RE";
+char * MENSAJE_DO = "NOTA MI";
+char * MENSAJE_DO = "NOTA FA";
+char * MENSAJE_DO = "NOTA SOL";
+char * MENSAJE_DO = "NOTA LA";
+char * MENSAJE_DO = "NOTA SI";
+
+int main (void){       
+    
+    iniPerifericos();
+    iniLCD8bits();
+    iniInterrupciones();
+    
+    int bp = 0;
+    
+    for(;EVER;){
+        
+        
+        Nop();
+    }
+
+    return 0;
+}
+/****************************************************************************/
+/* DESCRICION:	ESTA RUTINA INICIALIZA LAS INTERRPCIONES    				*/
+/* PARAMETROS: NINGUNO                                                      */
+/* RETORNO: NINGUNO															*/
+/****************************************************************************/
+
+/*void iniInterrupciones( void ){
+    //Habilitacion de interrupcion del periférico 1
+    //Habilitacion de interrupcion del periférico 2
+    //Habilitacion de interrupcion del periférico 3
+}*/
+
+/****************************************************************************/
+/* DESCRICION:	ESTA RUTINA INICIALIZA LOS PERIFERICOS						*/
+/* PARAMETROS: NINGUNO                                                      */
+/* RETORNO: NINGUNO															*/
+/****************************************************************************/
+void iniPerifericos( void ){
+    LATB = 0;           Nop();
+    TRISB = 0;          Nop();
+    PORTB = 0;          Nop();
+    ADPCFG=0xFFFF;      Nop();
+    
+    LATD = 0;           Nop();
+    TRISD = 0;          Nop();
+    PORTD = 0;          Nop();
+    
+    PORTF = 0;          Nop();
+    LATF = 0;           Nop();
+    TRISF = 0xFFFF;     Nop();
+}
+
+/********************************************************************************/
+/* DESCRICION:	ISR (INTERRUPT SERVICE ROUTINE) DEL TIMER 1						*/
+/* LA RUTINA TIENE QUE SER GLOBAL PARA SER UNA ISR								*/	
+/* SE USA PUSH.S PARA GUARDAR LOS REGISTROS W0, W1, W2, W3, C, Z, N Y DC EN LOS */
+/* REGISTROS SOMBRA																*/
+/********************************************************************************/
+void __attribute__((__interrupt__)) _T1Interrupt( void )
+{
+        IFS0bits.T1IF = 0;    //SE LIMPIA LA BANDERA DE INTERRUPCION DEL TIMER 1                      
+}
