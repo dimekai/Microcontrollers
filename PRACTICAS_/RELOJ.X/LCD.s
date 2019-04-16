c9f2ea35520fd531d0f7a4847066bc25a5e25c52
Angel Lopez Manriquez <sigma271@outlook.com>
2019-04-16 11:30:31 -0500

    Actualizacion, impresion usando byte[]
    No incrementan las unidades de la version anterior, aun.

---
+++ PRACTICAS_/RELOJ.X/LCD.s
@@ -0,0 +1,177 @@
+; |------------ CABECERA ------------|
+    .include "p30F4013.inc" ; Aqui estan todos los registros del micro.
+        
+; |------------ FUNCIONES LCD ------------|
+; Estas funciones son de los diagramas que definimos
+    .GLOBAL _comandoLCD	    
+    .GLOBAL _datoLCD
+    .GLOBAL _busyFlagLCD
+    .GLOBAL _iniLCD8bits
+    .GLOBAL _imprimeLCD
+    
+; |------------ EQUIVALENCIAS ------------|
+; Este es el equivalente a definicion de macros (#define) en C
+    .EQU    RS_LCD,	RD3 ; RS
+    .EQU    RW_LCD,	RD9 ; RW
+    .EQU    ENABLE_LCD, RD2 ; ENABLE
+    .EQU    BF_LCD,	RB7 ; BF: BUSY_FLAG
+    
+
+; |------------------ FUNCION COMANDO_LCD ------------------|
+_comandoLCD:
+    CLR	    TRISD
+    NOP
+    
+    BCLR    PORTD, #RS_LCD	; RS = 0
+    NOP
+    BCLR    PORTD, #RW_LCD	; RW = 0
+    NOP
+    BSET    PORTD, #ENABLE_LCD	; ENABLE = 1
+    NOP
+    
+    MOV.B   WREG,   PORTB	; PORTB = W0
+    NOP
+
+    BCLR    PORTD, #ENABLE_LCD	; ENABLE = 0
+    NOP
+  
+    RETURN
+	    
+; |------------------- FUNCION DATO LCD -------------------|    
+_datoLCD:
+;    CLR	    TRISF
+    CLR	    TRISD
+    
+    BSET    PORTD, #RS_LCD	;   RS = 1
+    NOP
+    BCLR    PORTD, #RW_LCD	;   RW = 0
+    NOP
+    BSET    PORTD, #ENABLE_LCD	;   ENABLE = 1
+    NOP
+    
+    MOV.B   WREG,   PORTB	;   PORTB = W0
+    NOP
+
+    BCLR    PORTD, #ENABLE_LCD	;   ENABLE = 0
+    NOP
+  
+    RETURN    
+
+; |------------------- FUNCION BUSY_FLAG -------------------|   
+_busyFlagLCD:
+    PUSH    W0
+;    CLR	    TRISF
+    CLR	    TRISD
+    
+    BCLR    PORTD,  #RS_LCD	;   RS = 0
+    NOP
+
+    SETM.B  TRISB		;   Prendemos la parte baja - TRISB OR 0X00FF
+    NOP
+
+    BSET    PORTD,  #RW_LCD	;   RW = 1
+    NOP
+
+    BSET    PORTD,  #ENABLE_LCD	;   ENABLE = 1
+    NOP
+
+PROCESO:
+    BTSC	PORTB,	#BF_LCD	;   VERIFICA SI BF = 0, SI NO, SE EJECUTA EL GOTO
+    GOTO	PROCESO
+    
+    BCLR	PORTD,	#ENABLE_LCD ;	ENABLE = 0
+    NOP
+    BCLR	PORTD, #RW_LCD	    ;   RW = 0
+    NOP
+    
+    SETM	TRISB		    ; TRISB = 0xFFFF
+    NOP
+    CLR.B	TRISB		    ; TRISB = 0xFF00 ; APAGA LA PARTE BAJA
+    NOP
+    
+    POP	    W0
+    RETURN;
+    
+; |------------------- FUNCION INICIALIZAR LCD DE 8 BITS -------------------|
+; | ---- INICIALIZACION ---|
+; | D7 | D6 | D5 | D4 | D3 |  D2 |  D1 |  D0 |     COMANDO    | CODIGO |
+; | 0  | 0  | 1  | 1  | X  |  X  |  X  |  X  |   FUNCION SET  |  0X30  |    ; RETARDO 01
+; | 0  | 0  | 1  | 1  | X  |  X  |  X  |  X  |   FUNCION SET  |  0X30  |    ; RETARDO 02
+; | 0  | 0  | 1  | 1  | X  |  X  |  X  |  X  |   FUNCION SET  |  0X30  |    ; RETARDO 03
+; | ---- CONFIGURACION ----|
+; | 0  | 0  | 1  | 1  | N=1| F=0 |  X  |  X  |   FUNCION SET  |  0X38  |
+; | 0  | 0  | 0  | 0  | 1  | D=0 | C=0 | B=0 | DISPLAY ON/OFF |  0X08  |
+; | 0  | 0  | 0  | 0  | 0  |  0  |  0  |  1  |  CLEAR DISPLAY |  0X01  |
+; | 0  | 0  | 0  | 0  | 0  |  1  |I/D=1| S=0 | ENTRY MODE SET |  0X06  |
+; | 0  | 0  | 0  | 0  | 1  | D=1 | C=1 | B=1 | DISPLAY ON/OFF |  0X0F  |
+    
+_iniLCD8bits:
+    CLR	    W0
+    ; ------- TABLA DE INICIALIZACION -------------
+    CALL    RETARDO_15ms	; -- RETARDO 01
+    MOV	    #0X30,  W0
+    CALL    _comandoLCD
+
+    CALL    RETARDO_15ms	; -- RETARDO 02
+    MOV	    #0X30,  W0
+    CALL    _comandoLCD
+    
+    CALL    RETARDO_15ms	; -- RETARDO 03
+    MOV	    #0X30,  W0
+    CALL    _comandoLCD
+    
+    ; ------- TABLA DE CONFIGURACION ---------------
+    CALL    _busyFlagLCD
+    MOV	    #0X38,  W0	    ;	CODIGO: 0X38 - FUNCTION SET
+    CALL    _comandoLCD
+    
+    CALL    _busyFlagLCD
+    MOV	    #0X08,  W0	    ;	CODIGO: 0X08 - DISPLAY ON/OFF
+    CALL    _comandoLCD
+    
+    CALL    _busyFlagLCD
+    MOV	    #0X01,  W0	    ;	CODIGO: 0X01 - CLEAR DISPLAY
+    CALL    _comandoLCD
+    
+    CALL    _busyFlagLCD
+    MOV	    #0X06,  W0	    ;	CODIGO: 0X06 - ENTRY MODE SET
+    CALL    _comandoLCD
+    
+    CALL    _busyFlagLCD
+    MOV	    #0X0F,  W0	    ;	CODIGO: 0X0F - DISPLAY ON/OFF
+    CALL    _comandoLCD
+    
+    RETURN
+
+; |=============== RETARDO ============|
+; @brief: Genera un retardo de 15ms
+RETARDO_15ms:
+    PUSH    W0 
+    CLR     W0
+CICLO1_1S:
+    DEC     W0,     W0
+    BRA     NZ,     CICLO1_1S
+
+    POP     W0
+    return
+
+    
+; |================== IMPRIMI EN LCD ==================|
+; |@brief: Imprime en el LCD una cadena de caracteres
+; |@param: Cadena de caracteres
+_imprimeLCD:
+    ; Se manda la direccion asociada a la variable
+    ; A partir de esto, utilizamos [] para tomar la direccion que se manda
+    PUSH    W1 
+    MOV	    W0,	    W1
+CICLO:
+    MOV.B   [W1++], W0	    ; Se utiliza .b ya que cada elemento, cada letra, es un byte
+    CP0.B   W0		    ; Compara el primer byte de W0 con 0
+    BRA	    Z, FIN	    ; Si ya llego al final, si es nulo, terminamos el programa
+    CALL    _busyFlagLCD
+    CALL    _datoLCD
+    GOTO    CICLO
+FIN:
+    POP	    W1
+    RETURN
+        
---
+++ PRACTICAS_/RELOJ.X/clk.s
@@ -0,0 +1,100 @@
+; Cabecera del DSPIC30F4013 -----------------------------------------
+.include "p30F4013.inc" ; Aqui estan todos los registros del micro.
+
+; Declaraciones globales 
+; interrupcion. 
+.GLOBAL __T1Interrupt   ; Interrupcion del timer 1
+
+; funciones
+.GLOBAL _clk_start       	; Turns the timer 1 on
+.GLOBAL _clk_remove_lock 	; It removes the clock for letting use a cristal as external clock source
+
+; Variables 
+.GLOBAL _dhr  ; decenas de hora
+.GLOBAL _uhr  ; unidades de hora
+.GLOBAL _dmin ; decenas de minuto
+.GLOBAL _umin ; unidad de minuto
+.GLOBAL _dseg ; decena de segundo
+.GLOBAL _useg ; unidad de segundo
+
+; Equivalencias
+;  .EQU    RS_LCD,	RF2 ; RS
+
+; Oscilador externo de 32768 Hz regexp(0b 1000 0000 0000 0000 = 0x 8000)
+
+; |------------ FUNCION DE INICIALIZACIÓN ------------|
+.GLOBAL _iniInterrupciones  ; Esta en C
+    
+_iniInterrupciones:
+    ; __T1Interrupt config
+    BCLR IFS0,      #INT1IF ; turn the flag off
+    BSET IEC0,      #INT1IE ; enable the interrupt for __T1Interrupt
+    return
+
+; Write a sequence of bits over w3 in order to be capable of using a real time clock provided by a crystal
+_clk_remove_lock:
+	push.s
+	mov.b 	#0x46, 		w1 		; follow write sequence
+	mov.b 	#0x57, 		w2 		; for OSCCONL writes
+	mov 	#OSCCONL, 	w3 		; w3 = &OSCCONL[0]
+	mov.b 	w1, 		[w3]		; IMPORTANT to write to write over mmap [w3]
+	mov.b 	w2, 		[w3]
+	bset 	OSCCONL, 	#LPOSCEN 	; enable 32kHz external xtal 
+	return
+
+_clk_start:
+	BSET	T1CON, 	#TON			; Activate Timer 1
+	pop.s
+	return
+
+; |================ ISR_T1 ================|
+; @brief: Genera el reloj por software.
+; DOS guiones bajos como prefijo denota interrupcion.
+__T1Interrupt:
+    PUSH	W0
+
+    MOV.B	#10,   W0
+    INC.B 	_useg 		; Segundos -----------------------------
+    CP.B	_useg		; Compare la variable con WREG
+    BRA 	NZ, 	FIN 
+    CLR.B 	_useg
+
+    MOV 	#6,    W0
+    INC.B	_dseg		    
+    CP.B	_dseg 		; NZ = !(6 - _dseg)
+    BRA		NZ,	FIN
+    CLR.B	_dseg		    
+    
+    MOV 	#10, 	W0
+    INC.B	_umin		; Minutos ------------------------------ 
+    CP.B	_umin		; NZ = !(10 - _umin)
+    BRA		NZ,	FIN
+    CLR.B	_umin		    
+    
+    MOV 	#6, 	W0
+    INC.B	_dmin		    
+    CP.B	_dmin 		; NZ = !(6 - _dmin)
+    BRA		NZ,	FIN
+    CLR.B	_dmin		    
+
+    MOV     	#10,	W0    	
+    BTSC 	_dhr,	#1	; W0 = (_dhr == 2) ? 4 : 10
+    MOV     	#4,	W0     
+
+    INC.B	_uhr 		; Horas --------------------------------
+    CP.B	_uhr	 	; NZ = !(Wn - f)
+    BRA		NZ,	FIN
+    CLR.B	_uhr		    
+
+    MOV		#3,	W0
+    INC.B	_dhr 	
+    CP.B	_dhr		; NZ = !(Wn - f)
+    BRA		NZ,	FIN
+    CLR.B	_dhr		    
+
+FIN:
+    BCLR    IFS0,   #INT1IF	; Turn down the appropiate flag for the executed interruption, very important
+    
+    POP	    W0
+    RETFIE
+    
